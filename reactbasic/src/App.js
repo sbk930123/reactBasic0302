bcb6146d2e0dc988017cee80b6f3c84338568095
sbk930123 <sbk930123@naver.com>
2023-03-03 11:40:14 +0900

    🧑‍💻

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState,} from 'react';
 
 function App() {
 
@@ -9,13 +9,16 @@ function App() {
   // react에서 변수처럼 자료를 보관할 수 있는 state문법
   let [count1, setCount1] = useState(0);
   let [count2, setCount2] = useState(0);
+  let [bgc, setBgc] = useState('white');
   // 변수에 들어있는 데이터를 html에 넣기
   // JSX 중괄호 문법 사용
+  
+
 
   let [title, setTitle] = useState(['DW아카데미 503호', 'DW아카데미 501호', 'DW아카데미 502호',]);
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor : bgc}}>
       <h1> Hello, {user} ! </h1>
       <p>This is a React App</p>
 
@@ -55,6 +58,10 @@ function App() {
         setTitle(sortedTitle);
       }}>클릭
       </button>
+
+      <button onClick={() => {
+        setBgc(bgc == 'white' ? 'aquamarine' : 'white');
+      }}>배경 변경</button>
 
     </div>
   );
