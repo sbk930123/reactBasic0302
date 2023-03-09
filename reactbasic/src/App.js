872605c47878e2ce469fb233a0032fffcec807a7
sbk930123 <sbk930123@naver.com>
2023-03-09 12:09:34 +0900

    🧑‍💻

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -6,11 +6,19 @@ import {useState} from 'react';
 import Box from './component/Box';
 import Comment from './component/Comment';
 import CommentList from './component/CommentList'
+import back from './pepe.jpg'
 
 function App() {
   return (
     <div className='App'>
       <CommentList/>
+      {/* <div className='back'></div> */}
+      {/* <div style={{backgroundImage : `url(${back})`, height : '500px'}}></div>
+      <img src= {process.env.PUBLIC_URL + 'pepe.jpg'} alt="" /> */}
+
+      <div style={{backgroundImage : 'url(https://i1.sndcdn.com/avatars-bjBRDNsPhAy1uXhU-2LstrA-t500x500.jpg)', height : '500px'}}></div>
+
+      <img src={back} alt="" />
     </div>
   )
 }
